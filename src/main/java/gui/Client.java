61553cb62b2ac008dadcd741400ef6a832c39d10
PLoic <loic.pauletto@gmail.com>
2015-01-14 16:32:09 +0100

    Update GUI

---
+++ src/main/java/gui/Client.java
@@ -0,0 +1,26 @@
+package gui;
+
+/**
+ * Created by  on 14/01/15.
+ */
+public class Client {
+
+    private String lastName;
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
