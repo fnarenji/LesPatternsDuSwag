01d520e80ebddc6cc04a3787ec32709cb9cd3a62
loick111 <loick111@gmail.com>
2015-01-15 16:22:11 +0100

    add ClientStage, SpotStage, VehiculeStage

---
+++ src/main/java/gui/ClientStage.java
@@ -0,0 +1,167 @@
+package gui;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by loick on 14/01/15.
+ */
+public class ClientStage extends Stage {
+
+    private Collection<String> civility_list = new ArrayList<>();
+
+    private Label title;
+    private Label label;
+    private ChoiceBox civility;
+    private TextField lastname;
+    private TextField firstname;
+    private Button submit;
+    private Button cancel;
+
+    private void createTitle() {
+        title = new Label("New Client");
+        title.setFont(Font.font("Arial", 30));
+        title.setTextFill(Color.BLACK);
+        title.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createLabel() {
+        label = new Label();
+        label.setTextFill(Color.RED);
+        label.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createCivilite() {
+        civility = new ChoiceBox();
+        civility.getItems().addAll(this.civility_list);
+        civility.setValue(this.civility_list.iterator().next());
+    }
+
+    private void createLastName() {
+        lastname = new TextField();
+        lastname.setPromptText("Last Name");
+
+        //style
+        lastname.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createFirstName() {
+        firstname = new TextField();
+        firstname.setPromptText("First Name");
+
+        //style
+        firstname.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createButtonCreate() {
+        submit = new Button();
+        submit.setText("Create");
+
+        //add action
+        submit.setOnAction(createSubmitEventHandler());
+
+        //style
+        submit.setStyle("-fx-background-color: green");
+        submit.setTextFill(Color.WHITE);
+    }
+
+    private void createButtonCancel() {
+        cancel = new Button();
+        cancel.setText("Cancel");
+
+        //add action
+        cancel.setOnAction(createCancelEventHandler());
+
+        //style
+        cancel.setStyle("-fx-background-color: red");
+        cancel.setTextFill(Color.WHITE);
+    }
+
+    private EventHandler createSubmitEventHandler() {
+        return event -> {
+            if(!lastname.getText().isEmpty()
+                    && !firstname.getText().isEmpty()
+                    ) {
+                this.close();
+            }else {
+                label.setText("Tous les champs ne sont pas renseignés");
+            }
+        };
+    }
+
+    private EventHandler createCancelEventHandler() {
+        return event -> this.close();
+    }
+
+    private void init() {
+        this.civility_list.add("M.");
+        this.civility_list.add("Mme");
+        this.civility_list.add("Mlle");
+
+        createTitle();
+        createLabel();
+        createCivilite();
+        createLastName();
+        createFirstName();
+        createButtonCreate();
+        createButtonCancel();
+    }
+
+    public ClientStage(Window owner) {
+        this.initOwner(owner);
+
+        init();
+
+        BorderPane borderPane = new BorderPane();
+        FlowPane flowPane = new FlowPane();
+
+        //add Nodes to FlowPane
+        flowPane.getChildren().addAll(
+                title,
+                civility,
+                lastname,
+                firstname,
+                submit,
+                cancel
+        );
+
+        flowPane.setMaxSize(200, 400);
+
+        //add FlowPane
+        flowPane.alignmentProperty().setValue(Pos.CENTER);
+        borderPane.setCenter(flowPane);
+        //add Label error
+        borderPane.setBottom(label);
+        borderPane.setAlignment(label, Pos.CENTER);
+
+        //create scene
+        Scene scene = new Scene(borderPane, 300, 200);
+
+        this.setResizable(false);
+        this.setScene(scene);
+        this.setTitle("New Client");
+    }
+
+    public Collection getClient() {
+        Collection client = new ArrayList<>();
+        client.add(civility.getValue());
+        client.add(lastname.getText());
+        client.add(firstname.getText());
+
+        return client;
+    }
+}
---
+++ src/main/java/gui/SpotStage.java
@@ -0,0 +1,131 @@
+package gui;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import parking.api.business.contract.ParkingSpot;
+
+/**
+ * Created by loick on 14/01/15.
+ */
+public class SpotStage extends Stage {
+
+    private ParkingSpot parkingSpot;
+    private Label title;
+    private Label id;
+    private Label state;
+    private Label booking;
+    private Button ok;
+
+    private void createTitle() {
+        title = new Label("Parking Spot");
+        title.setFont(Font.font("Arial", 30));
+        title.setTextFill(Color.BLACK);
+        title.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createID() {
+        id = new Label();
+
+        //set text
+        id.setText("ID : " + parkingSpot.getId());
+
+        id.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createState() {
+        state = new Label();
+
+        //set text
+        if(parkingSpot.isVehicleParked()) {
+            state.setText("Place Occupée");
+            state.setTextFill(Color.RED);
+        }
+        else {
+            state.setText("Place Libre");
+            state.setTextFill(Color.GREEN);
+        }
+
+        state.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createBooking() {
+        booking = new Label();
+
+        //set text
+        if(parkingSpot.isBooked()) {
+            booking.setText("Place Réservée");
+            booking.setTextFill(Color.RED);
+        }
+        else {
+            booking.setText("Place Non Réservée");
+            booking.setTextFill(Color.GREEN);
+        }
+
+        booking.alignmentProperty().setValue(Pos.CENTER);
+    }
+
+    private void createButtonOK() {
+        ok = new Button();
+        ok.setText("OK");
+
+        //add action
+        ok.setOnAction(createOKEventHandler());
+
+        //style
+        ok.setStyle("-fx-background-color: green");
+        ok.setTextFill(Color.WHITE);
+    }
+
+    private EventHandler createOKEventHandler() {
+        return event -> this.close();
+    }
+
+    private void init() {
+        createTitle();
+        createID();
+        createState();
+        createBooking();
+
+        createButtonOK();
+    }
+
+    public SpotStage(Window owner, ParkingSpot parkingSpot) {
+        this.initOwner(owner);
+        this.parkingSpot = parkingSpot;
+
+        init();
+
+        BorderPane borderPane = new BorderPane();
+        FlowPane flowPane = new FlowPane();
+
+        //add Nodes to FlowPane
+        flowPane.getChildren().addAll(
+                title,
+                state,
+                booking,
+                ok
+        );
+
+        flowPane.setMaxSize(200, 400);
+
+        //add FlowPane
+        flowPane.alignmentProperty().setValue(Pos.CENTER);
+        borderPane.setCenter(flowPane);
+
+        //create scene
+        Scene scene = new Scene(borderPane, 300, 200);
+
+        this.setResizable(false);
+        this.setScene(scene);
+        this.setTitle("Parking Spot");
+    }
+}
