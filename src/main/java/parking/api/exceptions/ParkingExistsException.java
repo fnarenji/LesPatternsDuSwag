527725fcd11935dfc1cc947a11a37254e3073e4d
oranthony <anthonyloroscio@gmail.com>
2015-01-17 23:54:56 +0100

    Commentaires

--- src/main/java/parking/api/exceptions/ParkingExistsException.java
+++ src/main/java/parking/api/exceptions/ParkingExistsException.java
@@ -4,6 +4,11 @@ package parking.api.exceptions;
  * Created by SKNZ on 28/12/2014.
  */
 public class ParkingExistsException extends ParkingException {
+    /**
+     * Raised when try create a parking with an id already existing
+     * 
+     * @param id Id of the parking
+     */
     public ParkingExistsException(Integer id) {
         super("Duplicate parking id (" + id + ")");
     }
