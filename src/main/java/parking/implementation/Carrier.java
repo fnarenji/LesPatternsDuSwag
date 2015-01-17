87d9b380b35bfafb0b859440e342bb3392d05456
sknz <florandara@gmail.com>
2015-01-17 18:37:45 +0100

    Add weight

--- src/main/java/parking/implementation/Carrier.java
+++ src/main/java/parking/implementation/Carrier.java
@@ -6,4 +6,13 @@ import parking.api.business.concrete.BaseVehicle;
  * Created by SKNZ on 31/12/2014.
  */
 class Carrier extends BaseVehicle {
+    private double weight;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
